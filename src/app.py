fbe692a085f02ce9457003e284229dfc036ae913
TulioPortela <156851372+Tulio-Portela@users.noreply.github.com>
2024-06-21 19:29:05 +0000

    listo

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    
+]
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+     return jsonify(todos)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
+
+  
